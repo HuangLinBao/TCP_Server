12369201be42a9f1f8f247ca5f84d041bc8728de
HuangLinBao <tariq.92@live.com>
2021-12-04 21:13:54 +0200

    fml

--- src/main/java/io/github/HuangLinBao/RedirectThread.java
+++ src/main/java/io/github/HuangLinBao/RedirectThread.java
@@ -6,19 +6,19 @@ public class RedirectThread extends Thread {
     @Override
     public void  run(){
         try {
-            ServerSocket welcomeSocket = new ServerSocket(Integer.parseInt(Controller.aux_port.getText()));
+            //ServerSocket welcomeSocket = new ServerSocket(Integer.parseInt(Controller.aux_port.getText()));
             while (true){
-                Socket connectionSocket = welcomeSocket.accept();
+                Socket connectionSocket = ServerThread.welcomeSocket.accept();
                 String message;
                 String[] msg;
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
                 message = inFromClient.readLine();
                 if(message.contains(",")){
                     msg = message.split(",");
                     Socket clientSocket = new Socket(msg[1],Integer.parseInt(msg[2]));
                     DataOutputStream outToPeer = new DataOutputStream(clientSocket.getOutputStream());
                     outToPeer.writeBytes(msg[0]);
+                    clientSocket.close();
                 }
 
             }
--- src/main/java/io/github/HuangLinBao/ServerThread.java
+++ src/main/java/io/github/HuangLinBao/ServerThread.java
@@ -5,20 +5,28 @@ import java.net.*;
 
 public class ServerThread extends Thread  {
 
-    public ServerThread() throws IOException {
+
+
+    static ServerSocket welcomeSocket;
+
+    static {
+        try {
+            welcomeSocket = new ServerSocket(Integer.parseInt(Controller.aux_port.getText()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @Override
     public void run(){
         try {
-            ServerSocket welcomeSocket = new ServerSocket(Integer.parseInt(Controller.aux_port.getText()));
             while (true){
                 Socket connectionSocket = welcomeSocket.accept();
                 String credentials;
                 String[] str;
                 String[] sent;
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
                 credentials = inFromClient.readLine();
                 if(credentials.contains("-")){
                     Controller.sb.append(credentials).append("\n");
@@ -26,9 +34,12 @@ public class ServerThread extends Thread  {
                    sent = Controller.sb.toString().split("\n");
                    for (int i = 0;i < sent.length;i++){
                        str = sent[i].split("-");
-                       Socket clientSocket = new Socket(str[1],Integer.parseInt(str[2]));
+                       System.out.println(str[1]);
+                       System.out.println(str[2]);
+                       Socket clientSocket = new Socket(str[1].trim(),Integer.parseInt(str[2].trim()));
                        DataOutputStream outToPeer = new DataOutputStream(clientSocket.getOutputStream());
                        outToPeer.writeBytes(Controller.sb.toString());
+                       clientSocket.close();
 
                    }
 
